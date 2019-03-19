00b146c5a02fb67986b7815302cf5d00d138efe3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-19 07:41:07 +0000

    Deploy eudoxos/cfitness to github.com/eudoxos/cfitness.git:gh-pages

--- epub/_static/documentation_options.js
+++ epub/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3db083f',
+    VERSION: 'e142031',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.xhtml',
